1975d3ae2b4bd8de645f28a4007fc9bb6f76da88
John David Tchomgui <tchomguijohn@gmail.com>
2024-11-14 08:52:14 +0100

    pfff

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+  resources :restaurants, only: [ :index, :new, :create, :show ] do
+    resources :reviews, only: [ :new, :create ]
+  end
   # Defines the root path route ("/")
   # root "posts#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Restaurant.create(name: "Le Gourmet", address: "123 Rue de Paris, Paris", category: "french")
+   Restaurant.create(name: "Sushi World", address: "456 Avenue des Japonais, Tokyo", category: "japanese")
+   Restaurant.create(name: "Italian Bistro", address: "789 Via Roma, Rome", category: "italian")
+   Restaurant.create(name: "Wok & Roll", address: "101 Rue de la Soie, Beijing", category: "chinese")
+   Restaurant.create(name: "Belgian Waffle House", address: "202 Rue des Gaufres, Bruxelles", category: "belgian")
